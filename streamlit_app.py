ab91798b94a2007cee6dd610f34d9c1582e5fd50
dustinvk03 <64234489+dustinvk03@users.noreply.github.com>
2025-04-12 18:23:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,9 +151,9 @@ if 'api_key_entered' not in st.session_state:
     st.session_state.api_key_entered = False
 
 # Define the password
-CORRECT_PASSWORD1 = PASSWORD1
-CORRECT_PASSWORD2 = PASSWORD2
-DEFAULT_API_KEY = API_KEY  # Your API Key
+# CORRECT_PASSWORD1 = PASSWORD1
+# CORRECT_PASSWORD2 = PASSWORD2
+# DEFAULT_API_KEY = API_KEY  # Your API Key
 
 # Gemini API Configuration
 def setup_gemini(api_key):
